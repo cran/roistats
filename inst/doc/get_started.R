13de6f09a02317d7c0f726a8fb403299dafe24ac
Yufei Zhao <fay.zz0213@gmail.com>
2021-03-10 16:20:02 +0000

    version 0.1.1

--- inst/doc/get_started.R
+++ inst/doc/get_started.R
@@ -11,30 +11,36 @@ library(roistats)
 ## ----example------------------------------------------------------------------
 head(color_index)
 
-## -----------------------------------------------------------------------------
-str(color_index)
+## ----message = FALSE----------------------------------------------------------
+library(dplyr)
 
-df_sem(color_index, color_index) # first arg refers the data.frame; second arg refers the coloumn
+color_index %>%
+  group_by(roi_id) %>% # The column to get summaries by
+  df_sem(color_index) # The column to summarize
 
-## -----------------------------------------------------------------------------
-library(magrittr) # No need to import magrittr if you have imported tidyverse already
-color_index_summary <- color_index %>%
-  df_sem(color_index)
-
-knitr::kable(color_index_summary, digits = 3)
+## ----warning = TRUE-----------------------------------------------------------
+df_sem(data = color_index, x = color_index)
 
 ## -----------------------------------------------------------------------------
-t_test_one_sample(color_index, "color_index", mu = 0)
+color_index %>% 
+  group_by(roi_id) %>% 
+  t_test_one_sample(color_index)
 
 ## -----------------------------------------------------------------------------
 color_index_one_sample_t_res <- color_index %>%
-  t_test_one_sample("color_index", mu = 0, p_adjust = c("bonferroni","fdr"))
+  group_by(roi_id) %>% 
+  t_test_one_sample(color_index, p_adjust = c("bonferroni", "BH"))
+
 knitr::kable(color_index_one_sample_t_res, digits = 3)
 
+## -----------------------------------------------------------------------------
+color_index %>% 
+  t_test_one_sample(color_index)
+
 ## ---- message = FALSE---------------------------------------------------------
-library(dplyr)
 color_index_one_sample_t_with_sig <- color_index_one_sample_t_res %>% 
   mutate(sig_origin_p = p_range(p))
+
 knitr::kable(color_index_one_sample_t_with_sig, digits = 3)
 
 ## -----------------------------------------------------------------------------
@@ -42,15 +48,9 @@ p_range(0.002)
 
 ## -----------------------------------------------------------------------------
 head(color_index_two_sample)
-str(color_index_two_sample)
 
 ## -----------------------------------------------------------------------------
-t_test_two_sample(color_index_two_sample, x = "color_effect", y = "group", paired = TRUE)
-
-## -----------------------------------------------------------------------------
-color_index_two_sample_t_res <- color_index_two_sample %>%
-  t_test_two_sample(
-    x = "color_effect", y = "group", paired = TRUE, p_adjust = c("bonferroni","fdr")
-  )
-knitr::kable(color_index_two_sample_t_res, digits = 3)
+color_index_two_sample %>% 
+  group_by(roi_id) %>% 
+  t_test_two_sample(color_effect, group, paired = TRUE)
 
